f605f4900467fd7e2ae4c797fa97c20961970d23
Igorxp5 <rogixp5@gmail.com>
2020-01-24 02:22:35 -0300

    CI: Add commit lint and publish release to Workflow
    - Add commit rules using commitizer

---
+++ .circleci/cz_applada/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='AppLadaCommitizen',
+    version='0.1.6',
+    py_modules=['cz_applada'],
+    license='MIT',
+    long_description='AppLada Commitizen Config',
+    install_requires=['commitizen']
+)
