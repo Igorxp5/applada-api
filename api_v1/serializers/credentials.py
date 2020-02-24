019097f2484b4c5c6df999faaf5a18f5d418475c
Igorxp5 <rogixp5@gmail.com>
2020-02-24 00:28:40 -0300

    FEAT: User's email is shown only for own user
    - Add user data in JWT payload
    
    Closes #23, closes #35

--- api_v1/serializers/credentials.py
+++ api_v1/serializers/credentials.py
@@ -1,6 +1,8 @@
 import rest_framework_simplejwt.serializers
 from rest_framework_simplejwt.serializers import PasswordField
 
+from api_v1.serializers import UserSerializer
+
 from django.utils.translation import gettext as _
 
 
@@ -10,8 +12,12 @@ class TokenObtainPairSerializer(rest_framework_simplejwt.serializers.TokenObtain
         'no_active_account': _('No active account found with the given credentials')
     }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        del token['user_id']  # remove default field in payload
+        token['user'] = UserSerializer(user).data
+        return token
 
 class TokenRefreshSerializer(rest_framework_simplejwt.serializers.TokenRefreshSerializer):
     pass
